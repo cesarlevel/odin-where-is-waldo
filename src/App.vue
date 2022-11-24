35e45d48dc73bd1665e963294e8a297de0a6be3a
Cesar Level <cesarlevel@gmail.com>
2022-11-24 17:19:56 -0500

    Basic event register

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,37 @@ import { useLoadChars, useGetDoc, useAddNewScore } from './firebase';
 
 export default {
   setup() {
+    const state = ref('start');
     const chars = ref([]);
     const doc = ref(null);
+    const mouseX = ref(0);
+    const mouseY = ref(0);
+    const showCharPicker = ref(false);
+    const hit = ref(false);
 
     onMounted(async () => {
       chars.value = await useLoadChars();
     });
+
+    function updatePos(e) {
+      showCharPicker.value = true;
+      const {offsetX, offsetY} = e;
+      const [x, y] = chars.value[0].mario
+      mouseX.value = `${offsetX}px`;
+      mouseY.value = `${offsetY}px`;
+    }
+
+    function checkChar(char){
+      if (chars.value[0]?.[char]) {
+        const [x, y] = chars.value[0][char];
+        const mX = +mouseX.value.replace('px', '');
+        const mY = +mouseY.value.replace('px', '');
+        const check = (x > mX - 40 && x < mX + 40) && (y > mY - 40 && y < mY + 40)
+        console.log(check);
+        hit.value = check;
+      }
+      showCharPicker.value = false;
+    }
 
     async function getDocument() {
       doc.value = await useGetDoc();
@@ -23,7 +48,13 @@ export default {
       chars,
       doc,
       getDocument,
-      addScore
+      addScore,
+      updatePos,
+      mouseX,
+      mouseY,
+      showCharPicker,
+      checkChar,
+      hit,
     };
   },
 }
@@ -31,16 +62,21 @@ export default {
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <p>{{hit}}</p>
+    <div class="image-container">
+      <template v-if="showCharPicker">
+        <div @click="showCharPicker = false" class="image-blocker"></div>
+        <div class="box" :style="`top: ${mouseY}; left: ${mouseX}`">
+          <button @click="checkChar('mario')">Mario</button>
+        </div>
+      </template>
+      <img @click="updatePos($event)" src="./assets/chars.png" />
+    </div>
     <p v-if="!chars.length">Loading</p>
     <p v-else>{{chars}}</p>
     <button @click="getDocument">Get Doc</button>
     <button @click="addScore">Add score</button>
+
     <p v-if="doc">{{doc}}</p>
   </div>
 </template>
